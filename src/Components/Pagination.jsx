8ba682c3da68116b002c0fc9e8b30e2cef17f9c3
Douglas Fernandes <fernandes.dod@gmail.com>
2023-09-09 21:22:36 -0300

    fix(Pagination.jsx).: correções linha 6, 7 e 19

--- src/Components/Pagination.jsx
+++ src/Components/Pagination.jsx
@@ -1,28 +1,30 @@
-// import React from "react";
+import React from "react";
 
-// const MAX_ITEMS = 9;
-// const MAX_LEFT =  (MAX_ITEMS - 1) / 2;
+const MAX_ITEMS = 9;
+const MAX_LEFT =  (MAX_ITEMS - 1) / 2;
 
-// const Pagination = ({ limit, total, offset }) => {
-//   const current = offset ? (offset / limit) * 1 : 1;
-//   const pages = Math.ceil(total / limit);
-//   const first = Math.max(current - MAX_LEFT, 1);
-//   return (
-//     <div>
-//       <ul>
-//         {Array.from({ length: MAX_ITEMS })
-//         .map((_, index) => index + first)
-//         .map((page) => (
-//           <li>
-//             <button 
-//               onClick={() => setOffset((page -1) * limit)}
-//             >
-//               {page}</button>
-//           </li>
-//         ))}
-//       </ul>
-//     </div>
-//   )
-// }
+const Pagination = ({ limit, total, offset, setOffset }) => {
+  const current = offset ? (offset / limit) + 1 : 1;
+  const pages = Math.ceil(total / limit);
+  const first = Math.max(current - MAX_LEFT, 1);
 
-// export default Pagination;
+  return (
+    <div>
+      <ul>
+        {Array.from({ length: MAX_ITEMS })
+        .map((_, index) => index + first)
+        .map((page) => (
+          <li>
+            <button 
+              onClick={() => setOffset((page - 1) * limit)}
+            >
+              {page}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default Pagination;
