57e893b6bc901633477cd9a97f69db3f5c864b13
Henrique Rocha <rochahenrique.1023@gmail.com>
2023-09-09 23:31:54 -0300

    more adds

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,10 +4,13 @@ import '../Css/Footer.css';
 function Footer() {
 	return (
 	  <footer className="footer">
-			<span>Linkedin</span>
-			<span>GitHub</span>
+			<a href="https://www.linkedin.com/in/henrique-rocha-de-sousa/" target="_Blank" rel="noreferrer">Linkedin</a>
+			<a href="https://github.com/rocha-henrique" target="_Blank" rel="noreferrer">GitHub</a>
 	  </footer>
 	);
 };
+
+
+
 
 export default Footer;
