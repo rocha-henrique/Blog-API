db40373e9eeb254766c0b89eaeb2e1106112ffc7
Douglas Fernandes <fernandes.dod@gmail.com>
2023-09-09 21:54:57 -0300

    feat(Home.jsx): alterando Home para incluir Pagination.

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from "react";
+import Pagination from "../Components/Pagination";
 import Posts from "../Components/Posts";
 import '../Css/Home.css'
 import axios from "axios";
 
 const Home = () => {
   const [posts, setPosts] = useState([]);
+  const [offset, setOffset] = useState(0);
   const apiEndPoint = 'https://jsonplaceholder.typicode.com/posts';
+  const LIMIT = 10;
+
   useEffect(() => {
     const getPosts = async () => {
       const { data: res } = await axios.get(apiEndPoint);
@@ -15,15 +19,24 @@ const Home = () => {
   },[]);
 
   console.log(posts);
+  console.log(posts.length);
   return (
     <div className="home">
       <h1>Home aaaaaaaaa</h1>
         <div>
             {
-              posts.map((post) => (
+              posts.slice(offset, offset + LIMIT).map((post) => (
                 <Posts body={post.body} title={post.title} />
               ))
             }
+        </div>
+        <div>
+          <Pagination 
+            limit={LIMIT}
+            total={posts.length}
+            offset={offset}
+            setOffset={setOffset}
+          />
         </div>
     </div>
   );
